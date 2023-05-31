fec7cb681704c81a62c9bf8e9bd6055fdac6426e
Daniel Geula <d.geula@gmail.com>
2023-05-31 04:03:09 +0100

    Update layout and add flagging
    Mostly cosmetic, but flagging could be useful for logging problematic queries.

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ import gradio as gr
 
 import utils
 
+# Define some example queries
+examples = [
+    ["How many players are there with a first name that begins with 'John'?"],
+    ["Is steph curry active?"],
+    ["List all players with a last name of Jordan"],
+]
 
+# Main function
 def nl_to_sql(prompt):
     
     # Translate the natural language query to SQL using the OpenAI API
@@ -18,27 +25,39 @@ def nl_to_sql(prompt):
         "descriptor_agent"
     )
 
-    return f"Query used: {sql_query}\n\n{query_description}", query_result
+    return query_description, sql_query, query_result
 
+
+""" gr.Interface method example
+app = gr.Interface(
+    nl_to_sql,
+    inputs=gr.Textbox(placeholder="What would you like to know?", lines=1, label="Question:"),
+    outputs=[gr.Textbox(lines=1, label="Data summary:"), gr.DataFrame(label="Data:")],
+    examples=examples,
+) """
+
+callback = gr.CSVLogger() # See docs: https://gradio.app/using-flagging/#flagging-with-blocks
 
 with gr.Blocks() as app:
 
-    # gr.Markdown("something something")
+    # gr.Markdown("App description")
 
     with gr.Row():
-        inp = gr.Textbox(placeholder="What would you like to know?", lines=1, label="Question:")
-        summ_out = gr.Textbox(lines=1, label="Data summary:")
+        with gr.Column(scale=2):
+            inp = gr.Textbox(placeholder="What would you like to know?", lines=1, label="Question:")
+            with gr.Row():
+                btn_run = gr.Button("Execute query")
+                btn_flag = gr.Button("Flag", variant="stop")
+            gr.Examples(examples, inp)
 
-    df_out = gr.DataFrame(label="Data:")
+        with gr.Column(scale=3):
+            summ_out = gr.Textbox(lines=1, label="Data summary:")
+            sql_out = gr.Textbox(lines=1, label="SQL query:")
+            df_out = gr.DataFrame()
 
-    btn = gr.Button("Execute query")
-    btn.click(fn=nl_to_sql, inputs=inp, outputs=[summ_out, df_out])
+    callback.setup([inp, summ_out, sql_out, df_out], "flagged_data_points")
 
-    gr.Examples(
-        ["How many players are there with a first name that begins with 'John'?",
-         "Is steph curry active?",
-         "List all players with a last name of Jordan?"],
-        inp
-    )
+    btn_run.click(fn=nl_to_sql, inputs=inp, outputs=[summ_out, sql_out ,df_out])
+    btn_flag.click(lambda *args: callback.flag(args), [inp, summ_out, sql_out, df_out], None, preprocess=False)
 
 app.launch()
