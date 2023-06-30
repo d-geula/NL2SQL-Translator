8f4b7c6bb26a0f9ff55ad3dedb13148ccdc62ff0
Daniel Geula <d.geula@gmail.com>
2023-06-30 19:21:52 +0100

    Create 'formula1' db from drivers.csv

---
+++ db/csv2db.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import sqlite3
+
+
+df = pd.read_csv("db/drivers.csv")
+
+# Replace '\N' values with NaN (null) values
+df.replace("\\N", pd.NA, inplace=True)
+
+# Define the column types for the table
+dtypes = {
+    "driverId": "INTEGER PRIMARY KEY",
+    "driverRef": "TEXT",
+    "number": "INTEGER",
+    "code": "TEXT",
+    "forename": "TEXT",
+    "surname": "TEXT",
+    "dob": "TEXT",
+    "nationality": "TEXT",
+    "url": "TEXT",
+}
+
+conn = sqlite3.connect("db/formula2.sqlite")
+
+# Write the DataFrame to an SQLite table
+df.to_sql("drivers", conn, index=False, if_exists="replace", dtype=dtypes)
+
+conn.close()
