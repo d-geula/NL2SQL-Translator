149ad0aab0b126da7b46e99384d322c032024cae
Daniel Geula <d.geula@gmail.com>
2023-07-08 00:45:51 +0100

    Switch to 'ChatOpenAI' class and add 'fake' llm for testing

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
 from pathlib import Path
 
 import gradio as gr
-from langchain import OpenAI, SQLDatabase, SQLDatabaseChain
-
-# Define some example queries
-examples = [
-    ["How many players are there with a first name that begins with 'John'?"],
-    ["Is steph curry active?"],
-    ["List all players with a last name of Jordan"],
-]
+from langchain import SQLDatabase, SQLDatabaseChain
+from langchain.chat_models import ChatOpenAI
+from langchain.llms.fake import FakeListLLM
 
 
-# Main function
-def nl_to_sql(prompt: str):
-    db_path = "files/nba.sqlite"
-    db = SQLDatabase.from_uri(f"sqlite:///{db_path}", sample_rows_in_table_info=2)
-    llm = OpenAI(temperature=0, verbose=False)  # type: ignore
+def nl_to_sql(input: str, _test: bool = False):
+    db_path = "db/formula1.sqlite"
+    db = SQLDatabase.from_uri(f"sqlite:///{db_path}", sample_rows_in_table_info=3)
+
+    if not _test:
+        llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, verbose=True)  # type: ignore
+    else:
+        response = [
+            'SELECT "forename", "surname" FROM drivers WHERE "nationality" = \'Swedish\' LIMIT 5;',
+            "Stefan Johansson, Slim Borgudd, Ronnie Peterson, Gunnar Nilsson, Conny Andersson",
+        ]
+        llm = FakeListLLM(responses=response)
 
     db_chain = SQLDatabaseChain.from_llm(
-        llm, db, verbose=False, return_intermediate_steps=True
+        llm, db, verbose=True, return_intermediate_steps=True
     )
 
-    result = db_chain(prompt)
+    result = db_chain(input)
 
     sql_query = result["intermediate_steps"][1]
     data = eval(result["intermediate_steps"][3])
@@ -29,6 +31,12 @@ def nl_to_sql(prompt: str):
 
     return summary, sql_query, data
 
+
+examples = [
+    ["List all swedish drivers?"],
+    ["How many drivers are there born after 1995?"],
+    ["Who is the youngest driver?"],
+]
 
 callback = (
     gr.CSVLogger()
@@ -59,7 +67,7 @@ with gr.Blocks() as app:
 
     btn_run.click(fn=nl_to_sql, inputs=inp, outputs=[summ_out, sql_out, df_out])
     btn_flag.click(
-        lambda *args: callback.flag(args),
+        lambda *args: callback.flag(args),  # type: ignore
         [inp, summ_out, sql_out, df_out],
         None,
         preprocess=False,
