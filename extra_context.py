2626e539430cd07f7e299be414287cd875ec7ee0
Daniel Geula <d.geula@gmail.com>
2023-07-28 04:59:27 +0100

    Update from main
    Also forgot to delete `extra_context`. It was only for testing purposes.

--- extra_context.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-from nl2sql.enahnced_summary import enhanced_summary
-
-docs = Path("docs/martin_brundle.txt").read_text()
-user_query, sql_results = ("Tell me about Martin Brundle", [("Martin", "Brundle", "1959-06-01", "British")])
-
-print(enhanced_summary(docs, user_query, sql_results))
